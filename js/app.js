ac4746edda128c560682f2a7a79f082fcbeac76b
BrisiaCastrejon <brissmonse@gmail.com>
2018-11-28 22:41:51 -0600

    Variables para desarrollo del juego

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+// botones del juego
+const botonAzul = document.getElementById('azul');
+const botonAmarillo = document.getElementById('amarillo');
+const botonNaranja = document.getElementById('naranja');
+const botonRosa = document.getElementById('rosa');
+
+ //boton de inicio
 const botonInicio = document.getElementById('btnEmpezar');
 
 botonInicio.addEventListener('click', empezarJuego)
